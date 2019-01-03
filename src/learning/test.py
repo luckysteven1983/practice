7414ea789ee6d140d2b1ea6cdbab47b5d1d41bf1
scndli <da.li@sas.com>
2019-01-03 13:55:23 +0800

    start from scratch

---
+++ src/learning/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding=utf-8
+print "你好"
+output = 'test1\n\
+test2\n\
+test3'
+print output
+
+Money = 2000
+def AddMoney():
+    # 想改正代码就取消以下注释:
+    global Money
+    Money = Money + 1
+
+
+print Money
+AddMoney()
+print Money
